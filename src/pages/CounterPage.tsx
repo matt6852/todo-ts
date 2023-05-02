c9d593c5a4dd0d58b9127734fd0dc82cbbc411f7
Сергей Мячин <s.myachin@Sergey-MacBook-Air.local>
2023-05-02 07:58:13 +0300

    practice simple useReducer

--- src/pages/CounterPage.tsx
+++ src/pages/CounterPage.tsx
@@ -1,18 +1,53 @@
-import React, { FormEvent } from 'react'
-import useConter from '../hooks/use-counter-hook'
+import React, { FormEvent, useReducer } from 'react'
+// import useConter from '../hooks/use-counter-hook'
 import Button from '../components/Button'
 
+const countReducer = (state: any, action: any) => {
+ if (action.type === "inc") {
+  return { ...state, count: state.count + 1 }
+ }
+ if (action.type === "dec") {
+  return { ...state, count: state.count - 1 }
+ }
+ if (action.type === "add") {
+  return { ...state, count: state.count + state.addValue, addValue: 0 }
+ }
+ if (action.type === "set") {
+  return { ...state, addValue: action.payload }
+ }
+
+ return state
+}
+const countState = {
+ count: 10,
+ addValue: 0
+}
+
 const CounterPage = () => {
- const { counter } = useConter(10)
+ const [state, dispatch] = useReducer(countReducer, countState)
  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
   e.preventDefault()
+  dispatch({ type: "add" })
  }
+
+ const increce = () => {
+  dispatch({ type: "inc" })
+ }
+ const decrice = () => {
+  dispatch({ type: "dec" })
+ }
+ const handleChange = (e: any) => {
+
+  dispatch({ type: "set", payload: +e.target.value || 0 })
+ }
+
  return (
   <div>
-   <h1>conuter: {counter}</h1>
+   <Button onClick={increce} primary>+</Button>
+   <Button onClick={decrice} danger>-</Button>
+   <h1>conuter: {state.count}</h1>
    <form onSubmit={handleSubmit} >
-
-    <div> <input className='border' type="number" /></div>
+    <div> <input onChange={handleChange} className='border' value={state.addValue || ""} type="number" /></div>
     <div> <Button warning rounded>submit</Button>
     </div>
 
