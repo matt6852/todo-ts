6d16f5fac26346ac95b9d7322f96a36a0cc44169
Сергей Мячин <s.myachin@Sergey-MacBook-Air.local>
2023-05-06 13:56:58 +0300

    make search comp

--- src/components/CarList.tsx
+++ src/components/CarList.tsx
@@ -3,15 +3,13 @@ import TotalCarPrice from './TotalCarPrice'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState, changeTerm, removeCar } from '../store'
 import Button from './Button'
+import SearchCar from './SearchCar'
 
 const CarList = () => {
  const { carList, serchTerm } = useSelector((state: RootState) => state.carList)
  const { name } = useSelector((state: RootState) => state.car)
  const dispatch = useDispatch()
- const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-  const value = e.target.value
-  dispatch(changeTerm(value))
- }
+
  const renderCars = carList.filter((car) => serchTerm ? car.name.includes(serchTerm) : car).map((car) => {
   return <div key={car.id} style={{ display: "flex", justifyContent: "space-between", padding: 5, alignItems: "center" }}>
    <div style={{ border: car.name === name ? "solid red 2px" : "" }} >Car name: {car.name} , price: {car.cost}$</div>
@@ -21,7 +19,8 @@ const CarList = () => {
  return (
   <div>
    <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
-    <div>My cars</div>  <div><input onChange={handleSearch} type="text" placeholder='sreac car' className='w-30 rounded-md border-2 py-1.5 pl-7 pr-20 text-gray-900 mx-10' /></div>
+    <div>My cars</div>
+    <SearchCar />
    </div>
    {renderCars}
    <div>
--- src/components/SearchCar.tsx
+++ src/components/SearchCar.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { changeTerm } from '../store'
 
 const SearchCar = () => {
+ const dispatch = useDispatch()
+ const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const value = e.target.value
+  dispatch(changeTerm(value))
+ }
  return (
   <div>
-   SearchCar
+   <input onChange={handleSearch} type="text" placeholder='sreac car' className='w-30 rounded-md border-2 py-1.5 pl-7 pr-20 text-gray-900 mx-10' />
   </div>
  )
 }
